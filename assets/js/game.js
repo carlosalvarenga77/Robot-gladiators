b62bf509c2d89818c9759ad5afb0cafdc2fff598
yungballerasscn <kurtzconrad77@gmail.com>
2021-05-21 03:38:35 -0500

    if and else

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,39 @@
-var playerName = window.prompt("What is your robot's name?");
-//What is this 
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-//this will do math and log 20
-console.log(10+10);
-//what is this?
-console.log("Our robot's name is " + playerName);
-function fight() {
-    window.alert("the fight has begun")
+var playerName= window.prompt("What is your robot's name?");
+var playerHealth= 100;
+var playerAttack= 50;
+
+//You can also long multiple values at once like this console.log(playerName, playerAttack, playerHealth)
+
+var enemyName = "Roborto";
+var enemyHealth = 10;
+var enemyAttack = 12;
+
+
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators");   
+
+enemyHealth = enemyHealth - playerAttack;
+
+console.log(playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining "
+);
+
+playerHealth = playerHealth - enemyAttack;
+
+console.log(
+    enemyName + " attacked " + playerName + " . " + playerName + " now has " +  playerHealth  + " health remaining. "
+);
+
+console.log(
+    playerName + " attacked " + enemyName + " . " + enemyName + " now has" + enemyHealth + " health remaining. "
+);
+
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died! ")
 }
-//fight();
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left. ")
+}
+};
+
+fight();
